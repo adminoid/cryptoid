46f3ce7165525810e3775198bd34c29ea29904df
Petr Zenin <mr@adminoid.com>
2019-11-01 10:23:16 +0300

    orders splitted to buy/sell

--- src/views/Analytics.vue
+++ src/views/Analytics.vue
@@ -2,7 +2,25 @@
   <div class="about">
     <h1>This is an orderBook analytics page</h1>
 
-    <b-table striped hover :items="orderBook"></b-table>
+    <div class="btn-group">
+      <button class="btn btn-primary" @click="startSync">Start</button>
+      <button class="btn btn-danger" :class="{ disabled: !started }" @click="stopSync">Stop</button>
+    </div>
+
+    <table class="table table-condensed">
+      <thead>
+      <tr>
+        <th>B</th>
+        <th>S</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>{{ orderBook.buy }}</td>
+        <td>{{ orderBook.sell }}</td>
+      </tr>
+      </tbody>
+    </table>
 
   </div>
 </template>
@@ -16,24 +34,27 @@
     data () {
       return {
         socketUrl: 'wss://www.bitmex.com/realtime',
-        orderBook: [],
+        orderBook: {
+          sell: [],
+          buy: [],
+        },
         socket: {},
+        started: false,
       }
     },
 
-    created() {
-      this.initSocket();
-    },
-
-    mounted() {
-
-      // setTimeout(() => {
-      //   this.socket.close();
-      // }, 7000);
-
-    },
-
     methods: {
+
+      stopSync() {
+        if (!this.started) return;
+        this.socket.close();
+        this.started = false;
+      },
+
+      startSync() {
+        this.started = true;
+        this.initSocket();
+      },
 
       initSocket() {
         this.socket = new WebSocket(this.socketUrl);
@@ -46,7 +67,19 @@
         this.socket.onmessage = function(event) {
           let response = JSON.parse(event.data);
           if (response.action === 'partial') {
-            self.initOrderBook(response.data);
+
+            _.map(response.data, order => {
+              if( order.side === 'Sell' ) {
+
+                self.orderBook.sell.push(order);
+
+              } else if (order.side === 'Buy') {
+
+                self.orderBook.buy.push(order);
+
+              }
+            });
+
           } else if (response.action === 'update') {
             const dataForUpdate = response.data;
 
@@ -58,16 +91,26 @@
       },
 
       update(newItem) {
-        let localItem = _.find(this.orderBook, ['id', newItem.id]);
-        _.merge(localItem, newItem);
-      },
 
-      initOrderBook (data) {
+        if (newItem.side === 'Sell') {
 
-        this.orderBook = data;
+          let localItem = _.find(this.orderBook.sell, ['id', newItem.id]);
+          _.merge(localItem, newItem);
+
+        } else {
+          let localItem = _.find(this.orderBook.buy, ['id', newItem.id]);
+          _.merge(localItem, newItem);
+        }
 
       },
 
     }
   }
 </script>
+
+<style lang="sass">
+
+  #main-table
+    font-size: 0.7em
+
+</style>
